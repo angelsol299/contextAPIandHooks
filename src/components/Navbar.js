2844f7f9a225376d86118c6b4e6a6b6411f0fddd
angel <angelsol299@hotmail.com>
2019-07-28 15:48:21 +0200

    consuming contextAPI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
   render() {
     return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLightTheme, light, dark } = context;
+      <AuthContext.Consumer>
+        {authContext => (
+          <ThemeContext.Consumer>
+            {themeContext => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
 
-          const theme = isLightTheme ? light : dark;
+              const theme = isLightTheme ? light : dark;
 
-          return (
-            <nav style={{ background: theme.ui, color: theme.sintax }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+              return (
+                <nav style={{ background: theme.ui, color: theme.sintax }}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? 'Logged in' : 'Logged out'}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
